5dbb16dae9fe21ca520a497d0fdb9790c90bd8be
Ryo Chiba <ryochiba@gmail.com>
2012-03-28 10:56:25 -0700

    corts second iteration

--- src/linkage/linkage.py
+++ src/linkage/linkage.py
@@ -5,6 +5,8 @@ Created on Mar 26, 2012
 '''
 from pymongo import Connection
 from nltk.metrics import edit_distance
+from nltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk
+
 class Linkage(object):
     def __init__(self):
         self.edit_dist_threshold = 0.2
@@ -17,7 +19,7 @@ class Linkage(object):
     def unescape_html_chars(self, item):
         return item.replace("&amp;", "&").replace("&gt;", ">").replace("&lt;", "<").replace("&quot;", "\"")
     
-    def extract_ne(self, item):
+    def extract_tweet_ne(self, item):
         item = item.split()
         # Split the annotated text on whitespace, collect B/I items and join them together
         # Then return a list of named entities
@@ -38,7 +40,24 @@ class Linkage(object):
         if len(cur_ne) > 0:
             result.append(" ".join(cur_ne))
         return result
-        
+    
+    def get_nearby_businesses(self, loc):
+        return [_ for _ in self.businesses.find({"loc":{"$maxDistance":2, "$near":loc}}).limit(20)]
+    
+    def get_nearby_photos(self, loc):
+        return [_ for _ in self.photos.find({"loc":{"$maxDistance":2, "$near":loc}}).limit(20)]
+    
+    def get_nearby_tweets(self, loc):
+        return [_ for _ in self.tweets.find({"loc":{"$maxDistance":2, "$near":loc}}).limit(100)]
+    
+    def extract_normal_ne(self, text):
+        result = []
+        for sent in sent_tokenize(text) if text else []:
+            for chunk in ne_chunk(pos_tag(word_tokenize(sent))):
+                if hasattr(chunk, "node"):
+                    result.append(" ".join([c[0] for c in chunk.leaves()]))
+        return result
+    
     def yelp_twitter_linkage(self):
         # Record the number of matches just for information
         num_matches = 0
@@ -48,14 +67,14 @@ class Linkage(object):
             if "annotated" not in tweet:
                 continue
             # Extract the named entities for comparison
-            named_entities = self.extract_ne(tweet["annotated"].encode("UTF-8"))
+            named_entities = self.extract_tweet_ne(tweet["annotated"].encode("UTF-8"))
             # Skip if we have no named entities in the tweet
             if not named_entities:
                 continue
             #print "Tweet loc: %s" % tweet["loc"]
             #print "NE: %s" % named_entities
             # Examine the 20 closest businesses
-            nearby_businesses = [_ for _ in self.businesses.find({"loc":{"$near":tweet["loc"]}}).limit(20)]
+            nearby_businesses = self.get_nearby_businesses(tweet["loc"])
             # Skip if there are no nearby businesses
             if not nearby_businesses:
                 continue
@@ -70,25 +89,100 @@ class Linkage(object):
                     # If it's the smallest seen so far, mark it
                     if edit_dist < min_dist:
                         min_dist = edit_dist
-                        best_match = business["name"]
+                        best_match = business
                 # If it's below the threshold(originally 20% of the length of the best match)
                 # then consider it a match
-                if min_dist <= self.edit_dist_threshold * len(best_match):
+                if min_dist <= self.edit_dist_threshold * len(best_match["name"]):
                     #print "Good match for %s is: %s --> edit dist is: %d" % (named_entity, best_match, min_dist)
                     num_matches += 1
                     # Mark it in the linked db
-                    self.linked.update({"id": "%s_%s" % (tweet["id"], business["id"])},
-                                       {"tweetID":tweet["id"], "yelpID":business["id"]}, True)
-        print "NUM MATCHES: %d" % num_matches
+                    self.linked.update({"id": "%s_%s" % (tweet["id"], best_match["id"])},
+                                       {"tweetID":tweet["id"], "yelpID":best_match["id"]}, True)
+        print "Yelp/Twitter: %d" % num_matches
 
     def yelp_flickr_linkage(self):
-        return
-    
+        num_matches = 0
+        for item in self.photos.find():
+            desc = item["description"].encode("UTF-8") if "description" in item and item["description"] else None
+            title = item["title"].encode("UTF-8") if "title" in item and item["title"] else None
+            if not desc and not title:
+                continue
+            named_entities = []
+            named_entities.extend(self.extract_normal_ne(desc))
+            named_entities.extend(self.extract_normal_ne(title))
+            #print "DESC: %s" % desc
+            #print "TITLE: %s" % title
+            #print "NE: %s" % ne_list
+            nearby_businesses = self.get_nearby_businesses(item["loc"])
+            if not nearby_businesses:
+                continue
+            # Similar to the function above, iterate through the named entities in the photos
+            # and compare to the Yelp business name. The best one, if within an edit distance
+            # limit, is considered a match.
+            for named_entity in named_entities:
+                #print "NE ITEM: %s" % ne
+                #print "NEARBY BUSINESSES: %s" % nearby_businesses
+                min_dist = self.MAX_EDIT_DISTANCE
+                best_match = None
+                for business in nearby_businesses:
+                    edit_dist = edit_distance(named_entity, business["name"])
+                    if edit_dist < min_dist:
+                        min_dist = edit_dist
+                        best_match = business
+                if min_dist <= self.edit_dist_threshold * len(best_match["name"]):
+                    num_matches += 1
+                    #print "Best match for %s is: %s" % (named_entity, best_match["name"])
+                    self.linked.update({"id":"%s_%s" % (item["id"], best_match["id"])},
+                                       {"flickrID":item["id"], "yelpID":best_match["id"]}, True)
+        
+        print "Yelp/Flickr matches: %d" % num_matches
+        
     def flickr_twitter_linkage(self):
-        return
+        num_matches = 0
+            
+        for tweet in self.tweets.find():
+            if "annotated" not in tweet:
+                continue
+            
+            named_entities = self.extract_tweet_ne(tweet["annotated"].encode("UTF-8"))
+            if not named_entities:
+                continue
+            
+            nearby_photos = self.get_nearby_photos(tweet["loc"])
+            if not nearby_photos:
+                continue
+            
+            for named_entity in named_entities:
+                min_dist = self.MAX_EDIT_DISTANCE
+                best_match = None
+                
+                for photo in nearby_photos:
+                    photo_named_entities = []
+                    desc = photo["description"].encode("UTF-8") if "description" in photo and photo["description"] else None
+                    title = photo["title"].encode("UTF-8") if "title" in photo and photo["title"] else None
+                    if not desc and not title:
+                        continue
+                    photo_named_entities.extend(self.extract_normal_ne(desc))
+                    photo_named_entities.extend(self.extract_normal_ne(title))
+                    
+                    for photo_named_entity in photo_named_entities:
+                        edit_dist = edit_distance(named_entity, photo_named_entity)
+                        if edit_dist < min_dist:
+                            min_dist = edit_dist
+                            best_match = photo
+                            
+                if min_dist < self.MAX_EDIT_DISTANCE and min_dist <= self.edit_dist_threshold * len(best_match):
+                    num_matches += 1
+                    #print "MATCH for tweet NE %s: flickr %s" % (named_entity, best_match)
+                    self.linked.update({"id":"%s_%s" % (tweet["id"], best_match["id"])},
+                                           {"tweetID":tweet["id"], "flickrID":best_match["id"]}, True)
+                        
+        print "Flickr/tweet matches: %s" % num_matches
     
 def main():
-    Linkage().yelp_twitter_linkage()
-    
+    l = Linkage()
+    l.yelp_twitter_linkage()
+    l.yelp_flickr_linkage()
+    l.flickr_twitter_linkage()
 if __name__=="__main__":
     main()
